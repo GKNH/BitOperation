89c56ec4f9d96ad25f786c8d879f0df93dff37ea
GKNH <15669986655@163.com>
2019-09-05 19:31:05 +0800

    添加枚举

--- 位运算/位运算/ViewController.m
+++ 位运算/位运算/ViewController.m
@@ -8,6 +8,13 @@
 
 #import "ViewController.h"
 
+typedef enum {
+    SUNOptionsOne = 1 << 0, // 0b0001
+    SUNOptionsTwo = 1 << 1, // 0b0010
+    SUNOptionsThree = 1 << 2, // 0b0100
+    SUNOptionsFour = 1 << 3 // 0b1000
+} SUNOptions;
+
 @interface ViewController ()
 
 @end
@@ -19,8 +26,22 @@
     // Do any additional setup after loading the view.
     // GitHub First Code
     
-    UIView *view = [[UIView alloc] init];
+    [self setOptions:0b0111];
 }
 
+- (void)setOptions:(SUNOptions)options {
+    if (options & SUNOptionsOne) {
+        NSLog(@"包含了MJOptionsOne");
+    }
+    if (options & SUNOptionsTwo) {
+        NSLog(@"包含了MJOptionsTwo");
+    }
+    if (options & SUNOptionsThree) {
+        NSLog(@"包含了MJOptionsThree");
+    }
+    if (options & SUNOptionsFour) {
+        NSLog(@"包含了MJOptionsFour");
+    }
+}
 
 @end
